25f5a534a6cf42c1016dddf41a144881d16de007
Anderson <vallejoanderson@gmail.com>
2022-07-31 19:19:56 -0500

    feat: telescope added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,3 +5,8 @@ vim.g.mapleader = ' '
 mapKeys("n", "<leader>q", ":q<CR>")
 mapKeys("n", "<leader>qq", ":q!<CR>")
 mapKeys("n", "<leader>w", ":w<CR>")
+
+vim.cmd [[ nnoremap <leader>sf <cmd>lua require('telescope.builtin').find_files()<cr> ]]
+vim.cmd [[ nnoremap <leader>sw <cmd>lua require('telescope.builtin').live_grep()<cr> ]]
+vim.cmd [[ nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr> ]]
+vim.cmd [[ nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr> ]]
